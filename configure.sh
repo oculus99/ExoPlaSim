e3231f02709c0e73ce596df4d3e177344d3fe1c2
Adiv Paradise <paradise@astro.utoronto.ca>
2017-10-16 22:28:00 -0400

    Initial commit; switching compilation to debug mode

--- configure.sh
+++ configure.sh
@@ -35,13 +35,13 @@ if [ $MOST_F90 != "NO_F90" ] ; then
       echo "gfortran version " $GFMAJ.$GFMIN
       # flags for gfortran version >= 4.9 [ -ffpe-summary ]
       if [ "$GFVER" -ge "409" ] ; then
-         MOST_F90_OPTS="-O3 -cpp -fcheck=all -ffixed-line-length-132 -ffpe-trap=invalid,zero,overflow -ffpe-summary=none -finit-real=zero"
+         MOST_F90_OPTS="-Og -ggdb3 -cpp -fcheck=all -ffixed-line-length-132 -ffpe-trap=invalid,zero,overflow -ffpe-summary=none -finit-real=zero"
          DEBUG_F90_OPTS="-g -O0 -ffpe-trap=invalid,zero,overflow -ffpe-summary=none -fcheck=all -finit-real=snan"
          echo > most_precision_options "MOST_PREC=-fdefault-real-8"
          PREC_OPTS="-fdefault-real-8"
       # flags for gfortran version 4.5 - 4.8
       elif [ "$GFVER" -ge "405" ] ; then
-         MOST_F90_OPTS="-O3 -cpp -fcheck=all -ffixed-line-length-132 -finit-real=zero -ffpe-trap=invalid,zero,overflow"
+         MOST_F90_OPTS="-Og -ggdb3 -cpp -fcheck=all -ffixed-line-length-132 -finit-real=zero -ffpe-trap=invalid,zero,overflow"
          DEBUG_F90_OPTS="-g -O0 -ffpe-trap=invalid,zero,overflow -fcheck=all -finit-real=snan"
          echo > most_precision_options "MOST_PREC=-fdefault-real-8"
          PREC_OPTS="-fdefault-real-8"
